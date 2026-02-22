4a8a1c5247939ba12bcab3ee947636ac4a0c1679
Adam Ashenfelter <ashenfad@gmail.com>
2026-02-22 14:02:24 -0800

    refactor: remove VirtualFile from public API

--- monkeyfs/__init__.py
+++ monkeyfs/__init__.py
@@ -5,7 +5,7 @@ from .config import FSConfig, IsolatedFSConfig, VirtualFSConfig, connect_fs
 from .context import current_fs, suspend
 from .isolated import IsolatedFS
 from .patching import patch
-from .virtual import VirtualFile, VirtualFS
+from .virtual import VirtualFS
 
 __all__ = [
     "connect_fs",
@@ -18,7 +18,6 @@ __all__ = [
     "IsolatedFSConfig",
     "patch",
     "suspend",
-    "VirtualFile",
     "VirtualFS",
     "VirtualFSConfig",
 ]
