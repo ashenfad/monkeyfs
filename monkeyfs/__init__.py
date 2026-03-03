52817903e7b8f81f62ea28615256848abab3c509
Adam Ashenfelter <ashenfad@gmail.com>
2026-03-02 16:33:14 -0800

    feat: export ReadOnlyFS and MountFS from package

--- monkeyfs/__init__.py
+++ monkeyfs/__init__.py
@@ -3,7 +3,9 @@
 from .base import FileInfo, FileMetadata, FileSystem
 from .context import current_fs, suspend
 from .isolated import IsolatedFS
+from .mount import MountFS
 from .patching import patch
+from .readonly import ReadOnlyFS
 from .virtual import VirtualFS
 
 __all__ = [
@@ -12,7 +14,9 @@ __all__ = [
     "FileMetadata",
     "FileSystem",
     "IsolatedFS",
+    "MountFS",
     "patch",
+    "ReadOnlyFS",
     "suspend",
     "VirtualFS",
 ]
